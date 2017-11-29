8c043421a37491b200b6838932a3c69b17b9998d
solersl <32671306+solersl@users.noreply.github.com>
2017-11-29 19:58:19 +0100

    Parte de los test de la práctica 1, los de las temperaturas y dat

--- Practica1.R
+++ Practica1.R
@@ -25,7 +25,14 @@ mitest<-t.test(datos_verano14$AVG.Temp.,datos_verano15$AVG.Temp.)
 #distribuciones sean la misma
 mivartest<-var.test(datos_verano14$AVG.Temp.,datos_verano15$AVG.Temp.)
 #El p-value asociado a la varianza es cercano a 1, luego no puedo rechazar la hipotesis de que las
-#dos distribuciones sean la misma por varianza
+#dos distribuciones sean la misma por varianza.
+
+#Histogramas
+hist(datos_verano14$AVG.Temp.)
+hist(datos_verano15$AVG.Temp.)
+
+#Las dos distribuciones son distintas, sin embargo el comportamiento es muy similar
+
 datos_verano<-datos[(datos$month==7 | datos$month==8 | datos$month==9),]
 #Media
 med<-mean(datos_verano$AVG.Temp.)
@@ -36,3 +43,37 @@ desv_est
 #Varianza
 varianza<-var(datos_verano$AVG.Temp.)
 varianza
+
+
+dat1<-read.table("dat1.DAT")
+dat2<-read.table("dat2.DAT")
+dat11<-dat1[!is.na(dat1)]
+dat22<-dat2[!is.na(dat2)]
+
+
+#Media
+mean(dat11)
+mean(dat22)
+
+#Desviacion estandar
+sd(dat11)
+sd(dat22)
+
+#Varianza
+var(dat11)
+var(dat22)
+
+testdat<-t.test(dat11,dat22)
+#p.value=1 Por lo que no podemos descartar que sean diferentes aunque tampoco
+#podemos garantizar que sean iguales.
+vartestdat<-var.test(dat11,dat22)
+#p. value nos sale cercano a uno por lo que no podemos garantizar que se trate de distribuciones
+#diferentes , pero tampoco garantizar que sean iguales.
+
+#Histogramas
+hist(dat11)
+hist(dat22)
+
+#Aunque los test no descarten que las distribuciones sean distintas en el histograma
+#podemos ver como en realidad las funciones son muy distintas.
+
